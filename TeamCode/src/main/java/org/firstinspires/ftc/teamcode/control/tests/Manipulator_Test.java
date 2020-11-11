8959bfbe4be7d8ed53df92ce597554102b7f9b1b
FTC-6078 <markortega663@gmail.com>
2020-11-11 11:51:32 -0500

    Fixed auto changes reverted
    Patrick Bradley
    11-11-2020

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/tests/Manipulator_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/tests/Manipulator_Test.java
@@ -59,7 +59,10 @@ public class Manipulator_Test extends LinearOpMode{
         runtime = new ElapsedTime();
 
        // intake = new Intake_Ring_Drop(hardwareMap.get(DcMotor.class, "intakeMotor"), hardwareMap.get(Servo.class, "intakeLockServo"));
-        shooter = new Shooter_Ring_ServoFed(hardwareMap.get(DcMotor.class, "shooterMotor"), hardwareMap.get(Servo.class, "feederServo"));
+        DcMotor shooterMoter = hardwareMap.get(DcMotor.class, "shooterMotor");
+        Servo feederServo = hardwareMap.get(Servo.class, "feederServo");
+        
+        shooter = new Shooter_Ring_ServoFed(shooterMoter, feederServo);
 
         telemetry.addData(robotName + "'s setup completed ", ")"); // Tell the user that robot setup has completed :)
         telemetry.update();
