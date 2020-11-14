7e870b0a103bf17c59c3c56ab5957db2861c900f
FTC-6078 <markortega663@gmail.com>
2020-11-14 14:36:22 -0500

    Fixed wobble arm problem
    Patrick Bradley
    11/14/2020

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/tests/TeleOpDriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/tests/TeleOpDriveTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.control.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.control.Provider2020;
@@ -76,7 +77,7 @@ public class TeleOpDriveTest extends LinearOpMode{
         runtime.reset(); // reset the clock once start has been pressed so runtime is accurate
 
 
-
+        robot.driveFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // The main run loop - write the main robot run code here
         while (opModeIsActive()) {
@@ -162,7 +163,7 @@ public class TeleOpDriveTest extends LinearOpMode{
                 telemetry.addData("Current Velocity", localizer.getPoseVelocity());
             }
 
-            robot.driveFL.getCurrentPosition();
+            telemetry.addData("Encoder reading", robot.driveFL.getCurrentPosition());
 
             telemetry.update();
         }
