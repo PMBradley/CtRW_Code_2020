56e625c0593150f39684fb21a97a6363fd666912
kessl <kesslerteague@gmail.com>
2020-12-07 19:42:20 -0500

    Kessler Teague 12/7/2020 Added the J_Shooter_Ring_ServoFed class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/shooter/J_Shooter_Ring_ServoFed.java
@@ -0,0 +1,116 @@
+package org.firstinspires.ftc.teamcode.hardware.shooter;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+public class J_Shooter_Ring_ServoFed {
+    private DcMotor shooterMotorFront;
+    private DcMotor shooterMotorBack;
+    private Servo feederServo;
+    private ElapsedTime localRuntime;
+
+    private static final double SHOOTER_RUN_POWER = 1.0;
+    private static final double SPIN_UP_TIME = 1000; // in milliseconds
+
+    private boolean firstSpinUp = false;
+    private boolean spunUp = false;
+    private double spinUpEndTime = 0;
+
+
+    private static final double FEEDER_EXTENDED_POSITION = 0.76;
+    private static final double FEEDER_RETRACTED_POSITION = 0.55;
+    private static final double FEEDER_EXTENSION_TIME = 650; // in milliseconds
+
+    private boolean isFiring = false;
+    private int firingState = 0;
+    private double moveStartTime = 0;
+
+
+    public J_Shooter_Ring_ServoFed(DcMotor shooterMotorFront, DcMotor shooterMotorBack, Servo feederServo){
+        this.shooterMotorFront = shooterMotorFront;
+        this.shooterMotorBack = shooterMotorBack;
+        this.feederServo = feederServo;
+        localRuntime = new ElapsedTime();
+    }
+
+
+    public boolean spinUp(){
+        shooterMotorFront.setPower( SHOOTER_RUN_POWER );
+        shooterMotorBack.setPower( SHOOTER_RUN_POWER );
+
+        if (firstSpinUp){
+            spinUpEndTime = localRuntime.milliseconds() + SPIN_UP_TIME;
+            firstSpinUp = false;
+        }
+        if(localRuntime.milliseconds() >= spinUpEndTime){
+            spunUp = true;
+        }
+
+        return spunUp;
+    }
+    public void spinDown(){
+        shooterMotorFront.setPower( 0.0 );
+        shooterMotorBack.setPower( 0.0 );
+
+        spunUp = false;
+        firstSpinUp = true;
+    }
+    public void setFlywheelMode( boolean isRunning ){
+        if(!isFiring){
+            if( isRunning ){
+                spinUp();
+            }
+            else {
+                spinDown();
+            }
+        }
+    }
+
+
+    public void instructFire(){
+        isFiring = true;
+    }
+
+    public void updateFeeder(){
+        if( isFiring ){
+
+            switch (firingState){
+                case 0:
+                    spinUp();
+                    if(isSpunUp()){
+                        moveStartTime = localRuntime.milliseconds();
+                        firingState = 1;
+                    }
+
+                    break;
+                case 1:
+                    feederServo.setPosition( FEEDER_EXTENDED_POSITION );
+
+                    if(localRuntime.milliseconds() >= moveStartTime + FEEDER_EXTENSION_TIME){
+                        firingState = 2;
+                    }
+                    break;
+                case 2:
+                    feederServo.setPosition( FEEDER_RETRACTED_POSITION );
+
+                    if(localRuntime.milliseconds() >= moveStartTime + (2*FEEDER_EXTENSION_TIME)){
+                        firingState = 0;
+                        isFiring = false;
+                    }
+                    break;
+            } // end of case switch
+        } // end of if
+    } // end of funtion
+
+    public int getFiringState(){
+        return firingState;
+    }
+    public boolean isSpunUp(){
+        return spunUp;
+    }
+}
+
+
+
