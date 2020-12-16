080c4513b09d85fde49171804bfd8cf8c043e525
FTC-6078 <markortega663@gmail.com>
2020-12-16 16:22:50 -0500

    Shooter feeding tuning
    Patrick Bradley
    12/14/2020

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/shooter/J_Shooter_Ring_ServoFed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/shooter/J_Shooter_Ring_ServoFed.java
@@ -26,9 +26,9 @@ public class J_Shooter_Ring_ServoFed {
 
     private double anglerPos = 0;
 
-    private static final double FEEDER_RETRACTED_POSITION = degToServoPos(101.0);// the feeder servo extened position
+    private static final double FEEDER_RETRACTED_POSITION = degToServoPos(104.0);// the feeder servo extened position
     private static final double FEEDER_EXTENDED_POSITION = degToServoPos(135.0);
-    private static final double FEEDER_EXTENSION_TIME = 130; // in milliseconds
+    private static final double FEEDER_EXTENSION_TIME = 140; // in milliseconds
 
     private static final double INDEXER_DOWN_POSITION = degToServoPos(134.0); // the little ring lifter down position
     private static final double INDEXER_UP_POSITION = degToServoPos(0.0);
