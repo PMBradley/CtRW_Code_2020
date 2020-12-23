6523cf593811c984c43c1bf3a30638172203b1e1
FTC-6078 <markortega663@gmail.com>
2020-12-22 22:10:13 -0500

    Additional Shooter modes
    Patrick Bradley
    12/22/2020

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/drive/Drive_Mecanum_Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/drive/Drive_Mecanum_Tele.java
@@ -18,9 +18,9 @@ public class Drive_Mecanum_Tele {
     private static final double DEFAULT_BOOSTING_MULTIPLIER = 1.0;
 
     // turn PID coeficients
-    public static final double Kp = 4.0;
+    public static final double Kp = 4.5;
     public static final double Ki = 0.0;
-    public static final double Kd = 0.0;
+    public static final double Kd = 1.0;
 
     private double lastError = 0.0;
     private double lastRuntime = 0.0;
