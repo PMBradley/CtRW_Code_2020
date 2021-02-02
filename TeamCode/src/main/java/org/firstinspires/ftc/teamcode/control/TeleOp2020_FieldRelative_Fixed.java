4f527fb17356118985faccea80f19c6134c90a51
PMBradley <44530443+PMBradley@users.noreply.github.com>
2021-02-02 17:17:17 -0500

    Name Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/TeleOp2020_FieldRelative_Fixed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/TeleOp2020_FieldRelative_Fixed.java
@@ -49,7 +49,7 @@ public class TeleOp2020_FieldRelative_Fixed extends LinearOpMode{
     // TeleOp Variables
 
     // Robot Name - Feel free to set it to whatever suits your creative fancy :)
-    String robotName = "Robot 2020";
+    String robotName = "Lil's ring flinga";
 
     // Robot Speed variables
     public static double turnSpeed = 0.50; // Speed multiplier for turning (1 being 100% of power going in) when not boosting
