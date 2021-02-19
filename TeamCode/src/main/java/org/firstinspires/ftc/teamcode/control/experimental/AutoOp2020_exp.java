2c9b103986ff92dff0c29fc864491ac47c0186ac
PMBradley <44530443+PMBradley@users.noreply.github.com>
2021-02-18 22:33:23 -0500

    7-8 ring
    Patrick Bradley
    2-18-2021

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/experimental/AutoOp2020_exp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/experimental/AutoOp2020_exp.java
@@ -49,14 +49,14 @@ public class AutoOp2020_exp extends LinearOpMode {
 
     public static TargetDrivePosition wobbleGoalPosA  = new TargetDrivePosition(18.0, -20.0, Math.toRadians(0.0)); // the positions that the robot needs to drive to
     public static TargetDrivePosition wobbleGoalPosB  = new TargetDrivePosition(14.0, -15.0, Math.toRadians(90.0));
-    public static TargetDrivePosition wobbleGoalPosC  = new TargetDrivePosition(42.4, -22.6, Math.toRadians(70.0), Math.toRadians(-85));
+    public static TargetDrivePosition wobbleGoalPosC  = new TargetDrivePosition(44, -24, Math.toRadians(70.0), Math.toRadians(-85));
     public static TargetDrivePosition wobblePickupPos = new TargetDrivePosition(-40.0, -22.2, Math.toRadians(-45.0), Math.toRadians(-135));
 
-    public static TargetDrivePosition lineShootPos = new TargetDrivePosition(-7, 0.0, Math.toRadians(0.0));
+    public static TargetDrivePosition lineShootPos = new TargetDrivePosition(-7, 0.0, Math.toRadians(-4));
     public static TargetDrivePosition powershot1Position = new TargetDrivePosition(-6.4, -13.6, Math.toRadians(15.2), Math.toRadians(-82));
     public static TargetDrivePosition powershot2Position = new TargetDrivePosition(-6.4, -2.2, Math.toRadians(15.2));
     public static TargetDrivePosition powershot3Position = new TargetDrivePosition(-6.4, 7.4, Math.toRadians(15.2));
-    public static TargetDrivePosition powerCollectStartPos = new TargetDrivePosition(47.8, 33, Math.toRadians(-30.0), Math.toRadians(-40));
+    public static TargetDrivePosition powerCollectStartPos = new TargetDrivePosition(47.7, 33, Math.toRadians(-30.0), Math.toRadians(-40));
     public static TargetDrivePosition powerCollectEndPos = new TargetDrivePosition(53, -9, Math.toRadians(-30.0), Math.toRadians(-90));
 
     //public static TargetDrivePosition stackPickupPos = new TargetDrivePosition(-34, -4.0, Math.toRadians(-160.0));
@@ -64,7 +64,7 @@ public class AutoOp2020_exp extends LinearOpMode {
     public static TargetDrivePosition ringPickupPos = new TargetDrivePosition(-40.5, -12.5, Math.toRadians(180.0));
     public static TargetDrivePosition parkPosA     = new TargetDrivePosition(2.0, -2.0, Math.toRadians(0.0));
     public static TargetDrivePosition parkPosB     = new TargetDrivePosition(2.0, -2.0, Math.toRadians(0.0));
-    public static TargetDrivePosition parkPosC     = new TargetDrivePosition(-5.0, -15.0, Math.toRadians(0.0));
+    public static TargetDrivePosition parkPosC     = new TargetDrivePosition(0.0, -15.0, Math.toRadians(0.0));
 
 
     public static double ARM_OFFSET_DEGREES = -300; // an offset for the wobble arm
