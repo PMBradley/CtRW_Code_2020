72d5d269d17a2a4436e1bfb027f4284c5329e489
PMBradley <44530443+PMBradley@users.noreply.github.com>
2021-02-18 21:26:37 -0500

    9 ring?
    Patrick Bradley
    2-18-2021

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/experimental/AutoOp2020_exp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/experimental/AutoOp2020_exp.java
@@ -55,7 +55,7 @@ public class AutoOp2020_exp extends LinearOpMode {
     public static TargetDrivePosition lineShootPos = new TargetDrivePosition(-7, 0.0, Math.toRadians(0.0));
     public static TargetDrivePosition powershot1Position = new TargetDrivePosition(-6.4, -13.6, Math.toRadians(15.2), Math.toRadians(-82));
     public static TargetDrivePosition powershot2Position = new TargetDrivePosition(-6.4, -2.2, Math.toRadians(15.2));
-    public static TargetDrivePosition powershot3Position = new TargetDrivePosition(-6.4, 6.5, Math.toRadians(15.2));
+    public static TargetDrivePosition powershot3Position = new TargetDrivePosition(-6.4, 7.4, Math.toRadians(15.2));
     public static TargetDrivePosition powerCollectStartPos = new TargetDrivePosition(47.8, 33, Math.toRadians(-30.0), Math.toRadians(-40));
     public static TargetDrivePosition powerCollectEndPos = new TargetDrivePosition(53, -9, Math.toRadians(-30.0), Math.toRadians(-90));
 
