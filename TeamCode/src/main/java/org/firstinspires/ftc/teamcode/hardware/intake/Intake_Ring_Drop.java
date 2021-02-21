fa049e825bdbef7c78c56edacea1feaffa9d2271
PMBradley <44530443+PMBradley@users.noreply.github.com>
2021-02-21 17:42:42 -0500

    Ring Gate
    Patrick Bradley
    2-21-2021

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/intake/Intake_Ring_Drop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/intake/Intake_Ring_Drop.java
@@ -16,8 +16,8 @@ public class Intake_Ring_Drop {
     private double currentIntakePower = 0.0;
 
 
-    public static double GATE_UP_POSITION   = degToServoPos(70.0);
-    public static double GATE_PREP_POSITION = degToServoPos(120.0);
+    public static double GATE_UP_POSITION   = degToServoPos(80.0);
+    public static double GATE_PREP_POSITION = degToServoPos(108.0);
     public static double GATE_DOWN_POSITION = degToServoPos(135.0);
     private String gatePosition = "UP";
 
